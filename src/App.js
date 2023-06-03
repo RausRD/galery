c76f190984affae6799fa20c98f6413ff3be8604
Ruslan Dunec <dunecruslan2@gmail.com>
2023-06-03 12:01:31 +0300

    створив логіку відображення фото з json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import './index.scss';
-
+import React from "react";
+import "./index.scss";
+import { Collection } from "./Collection";
 
 function App() {
+  const [collection, setCollection] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://647aea9bd2e5b6101db0a422.mockapi.io/photos")
+      .then((res) => res.json())
+      .then((json) => {
+        setCollection(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении даееых");
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -17,15 +31,9 @@ function App() {
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {collection.map((obj) => (
+          <Collection name={obj.name} images={obj.photos} />
+        ))}
       </div>
       <ul className="pagination">
         <li>1</li>
